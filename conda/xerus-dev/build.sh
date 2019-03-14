876b6404c0999e40f54f9a24d4baf591ef80b8f0
RoteKekse <goette@math.tu-berlin.de>
2019-03-14 14:19:41 +0100

    Update build.sh works for python and c++

--- conda/xerus-dev/build.sh
+++ conda/xerus-dev/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 cat <<EOF >config.mk
 CXX = ${CXX}
 COMPATIBILITY = -std=c++17
@@ -8,31 +7,41 @@ COMPILE_THREADS = 8                       # Number of threads to use during link
 HIGH_OPTIMIZATION = TRUE                  # Activates -O3 -march=native and some others
 OTHER += -fopenmp
 
+PYTHON3_CONFIG = `python3-config --cflags`
+PYTHON3_CONFIG += `python3-config --ldflags`
+
+
 LOGGING += -D XERUS_LOG_INFO              # Information that is not linked to any unexpected behaviour but might nevertheless be of interest.
 LOGGING += -D XERUS_LOGFILE               # Use 'error.log' file instead of cerr
 LOGGING += -D XERUS_LOG_ABSOLUTE_TIME     # Print absolute times instead of relative to program time
 
 INSTALL_LIB_PATH = ${PREFIX}/lib          # Path where to install the libxerus.so shared library.
 INSTALL_HEADER_PATH = ${PREFIX}/include   # Path where to install the xerus header files.
+INSTALL_PYTHON3_PATH = ${PREFIX}/lib/python${PY_VER}
 
 BLAS_LIBRARIES = -lopenblas -lgfortran    # Openblas, serial
 LAPACK_LIBRARIES = -llapacke -llapack     # Standard Lapack + Lapacke libraries
 SUITESPARSE = -lcholmod -lspqr
 BOOST_LIBS = -lboost_filesystem
 
+BOOST_PYTHON3 = -lboost_python37
+
 OTHER+= -L${PREFIX}/../_build_env/lib
 EOF
 
-# The following part is ugly!!!
-export CPP_INCLUDE_PATH=${PREFIX}/../_build_env/include
-export CPLUS_INCLUDE_PATH=${PREFIX}/../_build_env/include
-export CXX_INCLUDE_PATH=${PREFIX}/../_build_env/include
-export LIBRARY_PATH=${PREFIX}/../_build_env/lib
+export CPP_INCLUDE_PATH=${BUILD_PREFIX}/include:${BUILD_PREFIX}/lib/python3.7/site-packages/numpy/core/include/
+export CPLUS_INCLUDE_PATH=${BUILD_PREFIX}/include:${BUILD_PREFIX}/lib/python3.7/site-packages/numpy/core/include/
+export CXX_INCLUDE_PATH=${BUILD_PREFIX}/include:${BUILD_PREFIX}/lib/python3.7/site-packages/numpy/core/include/
+export LIBRARY_PATH=${BUILD_PREFIX}/lib
 
-mkdir -p ${PREFIX}/../_build_env/include/suitesparse/
-cp ${PREFIX}/../_build_env/include/*.h ${PREFIX}/../_build_env/include/suitesparse/
-cp ${PREFIX}/../_build_env/include/*.hpp ${PREFIX}/../_build_env/include/suitesparse/
-# ugly end
+mkdir -p ${BUILD_PREFIX}/include/suitesparse/
+
+cp ${BUILD_PREFIX}/include/*.h ${BUILD_PREFIX}/include/suitesparse/
+cp ${BUILD_PREFIX}/include/*.hpp ${BUILD_PREFIX}/include/suitesparse/
+
+mkdir -p ${PREFIX}/lib/python${PY_VER}
+
+make test -j4
 
 make install
 rm config.mk
