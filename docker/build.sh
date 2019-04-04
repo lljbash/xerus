dc3bfa64258654585c09976fb8b3ee455bf37e67
Philipp Trunschke <trunschk@math.hu-berlin.de>
2019-04-04 02:06:40 +0200

    update .sh scripts for docker management

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIRNAME=$(dirname $0)
-docker build -t firemarmot/base $DIRNAME/base
-docker build -t firemarmot/suitesparse $DIRNAME/suitesparse
-docker build -t firemarmot/xerus $DIRNAME/xerus
-docker build -t firemarmot $DIRNAME
+docker build "$@" -t firemarmot/base $DIRNAME/base
+docker build "$@" -t firemarmot/suitesparse $DIRNAME/suitesparse
+docker build "$@" -t firemarmot/xerus $DIRNAME/xerus
+docker build "$@" -t firemarmot $DIRNAME
