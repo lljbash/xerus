80d3f0796b82bc933f0bbfeaa2935f963f06eff4
Michael Goette <goette@navier.math.tu-berlin.de>
2019-04-25 14:54:06 +0200

    adds some more python tests

--- src/pyTests/test_tensor.py
+++ src/pyTests/test_tensor.py
@@ -5,27 +5,54 @@ import xerus as xe
 import numpy as np
 
 
-class TestArithmetic(unittest.TestCase):
+class TestTensor(unittest.TestCase):
     def setUp(self):
         self.dimension = [10, 15, 20]
         self.ranks = [1, 8, 4, 1]
 
     def test_create_tensors(self):
         ten_init = xe.Tensor()
+
         ten_init_dim = xe.Tensor([3,4,5])
         self.assertAlmostEqual(	ten_init_dim[0,0,0], 0)
+        self.assertAlmostEqual(	ten_init_dim.frob_norm(), 0)
+
         def fun(ind):
             return ind[0] + ind[1] + ind[2]   
         ten_init_fun = xe.Tensor.from_function([7,7,7], fun)
+        print(ten_init_fun.one_norm())
         self.assertAlmostEqual(	ten_init_fun[3,4,5], 12)        
         self.assertAlmostEqual(	ten_init_fun.order(), 3)
+        self.assertAlmostEqual(	ten_init_fun.frob_norm(), np.sqrt(31899))
+        self.assertAlmostEqual(	ten_init_fun.one_norm(),3087)
 
-    def test_tensor_linear_combination(self):
-        ten_a = xe.Tensor.ones(self.dimension)
-        ten_b = xe.Tensor.ones(self.dimension) * 4
-        ten_c = ten_a + ten_b
-        for (i, j, k) in ranges(*self.dimension):
-            self.assertAlmostEqual(ten_c[i, j, k], 5)
+        ten_init_ones = xe.Tensor.ones([2,3,2])
+        self.assertAlmostEqual(ten_init_ones.frob_norm(), np.sqrt(12))
+
+        ten_init_random = xe.Tensor.random([2,3,2])
+        self.assertGreater(ten_init_random.frob_norm(), 0)
+
+        ten_init_id = xe.Tensor.identity([2,3,2,3])
+        self.assertAlmostEqual(ten_init_id.frob_norm(), np.sqrt(6))
+
+        ten_init_kron = xe.Tensor.kronecker([2,3,2,3])
+        self.assertAlmostEqual(ten_init_kron[1,1,1,1], 1)
+        self.assertAlmostEqual(ten_init_kron[1,1,1,0], 0)
+
+        ten_init_dirac = xe.Tensor.dirac([2,3,2,3],[1,1,1,1])
+        self.assertAlmostEqual(ten_init_dirac[1,1,1,1], 1)
+        self.assertAlmostEqual(ten_init_dirac[1,1,1,0], 0)
+
+    def test_reinterpret_dimensions(self):
+        T = xe.Tensor.random([2,3,4,5])
+        T_new = T.dense_copy()
+        T.reinterpret_dimensions([6,20])
+        self.assertAlmostEqual(T[4,6], T_new[1,1,1,1])
+        
+                              
+
+
+
 
 
 
--- src/pyTests/test_tensor_arithmetic.py
+++ src/pyTests/test_tensor_arithmetic.py
@@ -5,21 +5,30 @@ import xerus as xe
 import numpy as np
 
 
-class TestTensor(unittest.TestCase):
+class TestTensorArithmetic(unittest.TestCase):
     def setUp(self):
         self.dimension = [10, 15, 20]
         self.ranks = [1, 8, 4, 1]
 
-    def test_create_tensors(self):
-        ten_init = xe.Tensor()
-        ten_init_dim = xe.Tensor([3,4,5])
-        self.assertAlmostEqual(	ten_init_dim[0,0,0], 0)
-        def fun(ind):
-            return ind[0] + ind[1] + ind[2]   
-        ten_init_fun = xe.Tensor.from_function([7,7,7], fun)
-        self.assertAlmostEqual(	ten_init_fun[3,4,5], 12)        
-        self.assertAlmostEqual(	ten_init_fun.order(), 3)
+   
+    def test_tensor_linear_combination(self):
+        ten_a = xe.Tensor.ones(self.dimension)
+        ten_b = xe.Tensor.ones(self.dimension) * 4
+        ten_c = ten_a + ten_b
+        for (i, j, k) in ranges(*self.dimension):
+            self.assertAlmostEqual(ten_c[i, j, k], 5)
 
-
-
-
+    def test_simple_multiplication(self):
+        ii,jj,kk = xe.indices(3)
+        ten_a = xe.Tensor.ones([2,2])
+        ten_b = xe.Tensor.ones([2,2]) * 2
+        ten_c = xe.Tensor()
+        
+        ten_c(ii/2,jj/2) << ten_a(ii/2,kk/2) * ten_b(kk/2,jj/2) 
+        self.assertAlmostEqual(ten_c.frob_norm(), 8)
+       
+        ten_c(ii&1,jj&1) << ten_a(ii&1,kk&1) * ten_b(kk&1,jj&1) 
+        self.assertAlmostEqual(ten_c.frob_norm(), 8)
+       
+        ten_c(ii^1,jj^1) << ten_a(ii^1,kk^1) * ten_b(kk^1,jj^1) 
+        self.assertAlmostEqual(ten_c.frob_norm(), 8)
