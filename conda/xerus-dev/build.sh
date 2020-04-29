c226f6c312ddf0513eaed3c9c2e2fda9b0f83757
RoteKekse <goette@math.tu-berlin.de>
2020-04-29 13:59:49 +0200

    Update build.sh

--- conda/xerus-dev/build.sh
+++ conda/xerus-dev/build.sh
@@ -14,6 +14,7 @@ PYTHON3_CONFIG += `python3-config --ldflags`
 LOGGING += -D XERUS_LOG_INFO              # Information that is not linked to any unexpected behaviour but might nevertheless be of interest.
 LOGGING += -D XERUS_LOGFILE               # Use 'error.log' file instead of cerr
 LOGGING += -D XERUS_LOG_ABSOLUTE_TIME     # Print absolute times instead of relative to program time
+XERUS_NO_FANCY_CALLSTACK = TRUE
 
 INSTALL_LIB_PATH = ${PREFIX}/lib          # Path where to install the libxerus.so shared library.
 INSTALL_HEADER_PATH = ${PREFIX}/include   # Path where to install the xerus header files.
@@ -24,11 +25,12 @@ LAPACK_LIBRARIES = -llapacke -llapack     # Standard Lapack + Lapacke libraries
 SUITESPARSE = -lcholmod -lspqr
 BOOST_LIBS = -lboost_filesystem
 
-BOOST_PYTHON3 = -lboost_python37
+BOOST_PYTHON3 = -lboost_python${CONDA_PY}
 
 OTHER+= -L${BUILD_PREFIX}/lib
 EOF
 
+printenv
 
 export CPP_INCLUDE_PATH=${BUILD_PREFIX}/include:${BUILD_PREFIX}/lib/python${PY_VER}/site-packages/numpy/core/include/
 export CPLUS_INCLUDE_PATH=${BUILD_PREFIX}/include:${BUILD_PREFIX}/lib/python${PY_VER}/site-packages/numpy/core/include/
@@ -37,8 +39,9 @@ export LIBRARY_PATH=${BUILD_PREFIX}/lib
 
 ln -s ${BUILD_PREFIX}/include/ ${BUILD_PREFIX}/include/suitesparse
 mkdir -p ${PREFIX}/lib/python${PY_VER}
+mkdir -p ${PREFIX}/include/
 
-make test -j4
+#make test -j4
 
 make install
 rm config.mk
